20c3c365fa83ff6766d30f4c4dd6cfc52f94958d
yang479 <78641256+yang479@users.noreply.github.com>
2021-03-10 16:15:01 +0800

    Delete Pratical5 directory

--- Pratical5/r_rate.py
+++
@@ -1,8 +0,0 @@
-r=1.2
-n=84
-i=1
-for i in range(1,6):
-  i=i+1
-  n=n*r
-print("The rate of reproduction of COVID-19 is"+ str(r))
-print("The total number of individuals infected after 5 generations is"+ str(n))
