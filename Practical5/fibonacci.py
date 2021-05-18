a4d2eec54da4501a99ffb656d1158d2156e24b85
yang479 <78641256+yang479@users.noreply.github.com>
2021-05-19 05:44:06 +0800

    Update fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,13 +1,13 @@
 a=1
 b=1
-i=2
+i=2 # input the default data
 print(a)
-print(b)
+print(b) #print a,b
 #a,b is the first two,dont ignore
-while i!=13:#dont ignore :
-  i=i+1
-  c=a+b
-  a=b
-  b=c
+while i!=13: #start the loop of Morphebonacci  #dont ignore ":"
+  i=i+1 #count the loops
+  c=a+b #the third number is the sum of the first and second number
+  a=b #the second number became the first number
+  b=c #the third number became the third number
   #all values, not the max
-  print(b)
+  print(b) #print the Morphebonacci sequence
