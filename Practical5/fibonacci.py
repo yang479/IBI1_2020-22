652d1493021b9c3b971875206f1553bd4cea23f3
yang479 <78641256+yang479@users.noreply.github.com>
2021-05-19 05:46:08 +0800

    Update fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -2,7 +2,7 @@ a=1
 b=1
 i=2 # input the default data
 print(a)
-print(b) #print a,b
+print(b) #print a,b(the first two number)
 #a,b is the first two,dont ignore
 while i!=13: #start the loop of Morphebonacci  #dont ignore ":"
   i=i+1 #count the loops
