9d0421c74da578506fa0cee4ac91bb649d3909fa
yang479 <78641256+yang479@users.noreply.github.com>
2021-05-19 11:11:06 +0800

    Add files via upload

--- practical14/run.py
+++ practical14/run.py
@@ -10,7 +10,7 @@ import re
 
 import sys
 sys.setrecursionlimit(500000) #increase the number of recursion
-#import things
+
 
 DOMTree = xml.dom.minidom.parse("go_obo.xml") #open the file
 xnl = DOMTree.documentElement
@@ -20,7 +20,6 @@ terms = xnl.getElementsByTagName("term") #select all terms
 Dic={}
 #set the default data
 
-#calculate the number of childnodes: this part taking example by Wang Bing
 for term in terms: #collect all childnodes and ids in terms
 	id=term.getElementsByTagName('id')
 	Dic[id[0].firstChild.data]=[]
@@ -30,6 +29,28 @@ for term in terms:
 	for is_a in is_as: #link the childnodes and ids to establish the dictionaty
         	Dic[is_a.firstChild.data].append(id[0].firstChild.data)
 #print(Dic)
+
+'''
+def count(a):
+    for term in terms:
+        if a in term.getElementsByTagName('defstr')[0].firstChild.data:#if 'a' exist in defstr, we thought we find what we need
+            ids=term.getElementsByTagName('id')[0].firstChild.data#get the id information
+            if dictionary[ids] !=[]:
+                counter=dictionary[ids]
+                n=count2(counter)
+    print('the childnode number of '+a+' is '+str(n))
+    return n
+def count2(counter):
+    for i in range(len(counter)):
+        if counter[i] not in listf:
+            listf.append(counter[i])#put the childnodes into listf
+            count2(dictionary[counter[i]])#put the 'grandchildnodes' into listf
+    return len(listf)
+listf=[]#listf is used to store the childnodes
+'''
+
+
+
 
 def measure(what):
 	number=0
@@ -63,15 +84,15 @@ carbohydrate=measure('carbohydrate') #I chose carbohydrate#I chose carbohydrate
 print('the number of childnodes of carbohydrate is',str(carbohydrate))
 
 #draw the pie chart
-DNA=DNA/(DNA+RNA+protein+carbohydrate)
-RNA=RNA/(DNA+RNA+protein+carbohydrate)
-protein=protein/(DNA+RNA+protein+carbohydrate)
-carbohydrate=carbohydrate/(DNA+RNA+protein+carbohydrate)
+DNAp=DNA/(DNA+RNA+protein+carbohydrate)
+RNAp=RNA/(DNA+RNA+protein+carbohydrate)
+proteinp=protein/(DNA+RNA+protein+carbohydrate)
+carbohydratep=carbohydrate/(DNA+RNA+protein+carbohydrate)
 #caculate the ratio
 #I doubt whether it is necessary, because the pie chart will calculate the ratio automatically in my version of python.
-substances=[DNA,RNA,protein,carbohydrate]  #input the data
-labels='DNA','RNA','protein','carbohydrate' #label the pie chart
+substances=[DNAp,RNAp,proteinp,carbohydratep] #input the data
+labels='DNA','RNA','protein','carbohydrate' #label the name
 plt.pie(substances,explode=(0.1,0.1,0,0.1),labels=labels,autopct='%1.2f%%',shadow=True,startangle=90) #draw the pie chart
-plt.title("The childnode number of DNA, RNA, protein and carbohydrate") #add the title 
-plt.show() #print
+plt.title("The childnode number of DNA, RNA, protein and carbohydrate") #add the title
+plt.show() #print it
 
